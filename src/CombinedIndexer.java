86798c1d3931f7d42f95bbbb8d4916b4625395af
Benjamin Vetter <vetter@flakks.com>
2012-06-06 17:50:31 +0200

    Shortened code and full paths.

--- src/CombinedIndexer.java
+++ src/CombinedIndexer.java
@@ -9,29 +9,32 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.analysis.WhitespaceAnalyzer;
 
 public class CombinedIndexer {
 	private static IndexWriter writer = null;
-	private static DocumentBuilder builder = null;
+    private static DocumentBuilder builder = null;
 	
-	public static final String FIELD_ID = "id";
-	public static final String FIELD_TEXT = "text";
+    public static final String FIELD_ID = "id";
+    public static final String FIELD_TEXT = "text";
 	
     public void index(String identifier, InputStream stream, String id, String text) throws IOException {
-    	if(CombinedIndexer.builder == null)
-    		CombinedIndexer.builder = DocumentBuilderFactory.getColorLayoutBuilder();
-     
-    	if(CombinedIndexer.writer == null)
-    		CombinedIndexer.writer = new IndexWriter(FSDirectory.open(new File("data")), new WhitespaceAnalyzer(), !(new File("data").exists()), IndexWriter.MaxFieldLength.UNLIMITED);
+        if(builder == null)
+            builder = DocumentBuilderFactory.getColorLayoutBuilder();
 
-        Document doc = CombinedIndexer.builder.createDocument(stream, identifier);
+        if(writer == null)
+            writer = new IndexWriter(FSDirectory.open(new File("data")), new WhitespaceAnalyzer(), !(new File("data").exists()), IndexWriter.MaxFieldLength.UNLIMITED);
+
+        writer.deleteDocuments(new Term(FIELD_ID, id));
+        writer.commit();
+
+        Document doc = builder.createDocument(stream, identifier);
         
         doc.add(new Field("text", text, Field.Store.YES, Field.Index.ANALYZED));
         doc.add(new Field("id", id, Field.Store.YES, Field.Index.NOT_ANALYZED));
         
-        CombinedIndexer.writer.addDocument(doc);
-        
-        CombinedIndexer.writer.commit();
+        writer.addDocument(doc);
+        writer.commit();
     }
 }
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -classpath jetty/*.jar:lire/commons-math-2.2.jar:lire/JOpenSurf.jar:lire/lire.jar:lire/lucene-core-3.4.0.jar:jetty/jetty-continuation-7.6.2.v20120308.jar:jetty/jetty-http-7.6.2.v20120308.jar:jetty/jetty-io-7.6.2.v20120308.jar:jetty/jetty-security-7.6.2.v20120308.jar:jetty/jetty-server-7.6.2.v20120308.jar:jetty/jetty-servlet-7.6.2.v20120308.jar:jetty/jetty-servlets-7.6.2.v20120308.jar:jetty/jetty-util-7.6.2.v20120308.jar:jetty/servlet-api-2.5.jar:bin Similarity
+/usr/bin/java -classpath jetty/*.jar:lire/commons-math-2.2.jar:lire/JOpenSurf.jar:lire/lire.jar:lire/lucene-core-3.4.0.jar:jetty/jetty-continuation-7.6.2.v20120308.jar:jetty/jetty-http-7.6.2.v20120308.jar:jetty/jetty-io-7.6.2.v20120308.jar:jetty/jetty-security-7.6.2.v20120308.jar:jetty/jetty-server-7.6.2.v20120308.jar:jetty/jetty-servlet-7.6.2.v20120308.jar:jetty/jetty-servlets-7.6.2.v20120308.jar:jetty/jetty-util-7.6.2.v20120308.jar:jetty/servlet-api-2.5.jar:bin Similarity
 
