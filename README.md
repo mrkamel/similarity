62c66c99ddde8b96c3510f433a331c4677d4d629
Benjamin Vetter <vetter@flakks.com>
2012-08-12 12:32:39 +0200

    Properly generating xml, readme extended.

--- README.md
+++ README.md
@@ -12,7 +12,33 @@ First, you need to start the server:
 $ sh start.sh
 </pre>
 
-Afterwards you can index some images using e.g. Ruby and the RestClient gem:
+## Index and query using cURL
+
+After you started similarity, you can index some images using cURL:
+
+<pre>
+$ curl http://localhost:8984/uploads -F file=@/path/to/file.jpg -F "text=keyword1 keyword2" -F id=1
+...
+</pre>
+
+Finally, you can search for similary images:
+
+<pre>
+$ curl http://localhost:8984/search -F file=@/path/to/reference.jpg -F q=keyword1 -F start=0 -F limit=10
+&lt;response num="2"&gt;
+  &lt;result&gt;
+    &lt;id&gt;1&lt;/id&gt;
+    &lt;identifier&gt;thu-p3030062.jpg&lt;/identifier&gt;
+    &lt;text&gt;keyword1 keyword2&lt;/text&gt;
+    &lt;score&gt;1.0&lt;/score&gt;
+  &lt;/result&gt;
+  ...
+&lt;/response&gt;
+</pre>
+
+## Index and query using RestClient
+
+After you started similarity, you can index some images using e.g. Ruby and the RestClient gem:
 
 <pre>
 gem install rest-client
@@ -20,13 +46,25 @@ gem install rest-client
 $ irb
 irb> require "rubygems"
 irb> require "rest-client"
-irb> RestClient.post("http://localhost:8984/uploads", :file => File.new("test.jpg"), :text => "keywords", :id => 1) 
+irb> RestClient.post("http://localhost:8984/uploads",
+  :file => File.new("/path/to/file.jpg"), :text => "keyword1 keyword2 ...", :id => 1) 
+...
 </pre>
 
 Finally, you can search for similar images:
 
 <pre>
-irb> RestClient.post("http://localhost:8984/search", :file => File.new("reference.jpg"), :q => "keywords", :start => 0, :limit => 2)
-=> "&lt;response num='291'&gt;\n&lt;result&gt;&lt;id&gt;1&lt;/id&gt;&lt;identifier&gt;test.jpg&lt;/identifier&gt;&lt;text&gt;keywords&lt;/text&gt;&lt;score&gt;0.46990407&gt;/score&gt;&lt;/result&gt;...&lt;/response&gt;\n"
+irb> puts RestClient.post("http://localhost:8984/search", :file => File.new("reference.jpg"),
+  :q => "keyword1", :start => 0, :limit => 10)
+&lt;response num="1"&gt;
+  &lt;result&gt;
+    &lt;id&gt;1&lt;/id&gt;
+    &l;identifier&gt;thu-p3030062.jpg&gt;/identifier&gt;
+    &lt;text&gt;abc def&lt;/text&gt;
+    &lt;score&gt;1.0&lt;/score&gt;
+  &lt;/result&gt;
+  ...
+&lt;/response&gt;
+=> nil
 </pre>
 
--- src/SearchHandler.java
+++ src/SearchHandler.java
@@ -4,15 +4,28 @@ import net.semanticmetadata.lire.DocumentBuilder;
 import java.io.IOException;
 import java.io.FileInputStream;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import net.semanticmetadata.lire.ImageSearchHits;
 
 import org.apache.lucene.queryParser.ParseException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class SearchHandler extends HttpServlet {	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -25,27 +38,61 @@ public class SearchHandler extends HttpServlet {
 			int start = new Integer(request.getParameter("start")).intValue();
 			int limit = new Integer(request.getParameter("limit")).intValue();
 			
-			PrintWriter writer = response.getWriter();
-			
-			writer.println("<response num='" + hits.length() + "'>");
-			
+			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+				
+			Element response_element = document.createElement("response");
+			response_element.setAttribute("num", new Integer(hits.length()).toString());
+			document.appendChild(response_element);
+
 			if(start < hits.length()) {
 				for(int i = start; i < Math.min(start + limit, hits.length()); i++) {
 					String id = hits.doc(i).getField(CombinedIndexer.FIELD_ID).stringValue();
 					String identifier = hits.doc(i).getField(DocumentBuilder.FIELD_NAME_IDENTIFIER).stringValue();
 					String text = hits.doc(i).getField(CombinedIndexer.FIELD_TEXT).stringValue();
-					
-					writer.println("<result>");
-						writer.println("<id>" + id + "</id>");
-						writer.println("<identifier>" + identifier + "</identifier>");
-						writer.println("<text>" + text + "</text>");
-						writer.println("<score>" + hits.score(i) + "</score>");
-					writer.println("</result>");
+						
+					Element result_element = document.createElement("result");
+					response_element.appendChild(result_element);
+						
+					Element id_element = document.createElement("id");
+					id_element.appendChild(document.createTextNode(id));
+					result_element.appendChild(id_element);
+						
+					Element identifier_element = document.createElement("identifier");
+					identifier_element.appendChild(document.createTextNode(identifier));
+					result_element.appendChild(identifier_element);
+						
+					Element text_element = document.createElement("text");
+					text_element.appendChild(document.createTextNode(text));
+					result_element.appendChild(text_element);
+						
+					Element score_element = document.createElement("score");
+					score_element.appendChild(document.createTextNode(new Float(hits.score(i)).toString()));
+					result_element.appendChild(score_element);
 				}
 			}
+
+			PrintWriter writer = response.getWriter();
+				
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
 			
-			writer.println("</response>");
-		} catch (ParseException e) {
+			StringWriter string_writer = new StringWriter();
+			StreamResult stream_result = new StreamResult(string_writer);
+	        DOMSource source = new DOMSource(document);
+	        transformer.transform(source, stream_result);
+	            
+			writer.println(string_writer.toString());
+		} catch(ParseException e) {
+			e.printStackTrace();
+		} catch(TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch(TransformerFactoryConfigurationError e) {
+			e.printStackTrace();
+		} catch(TransformerException e) {
+			e.printStackTrace();
+		} catch(ParserConfigurationException e) {
 			e.printStackTrace();
 		}
 	}
