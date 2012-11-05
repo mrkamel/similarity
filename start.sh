326767e44cdfeebf718fb8715dbf68d0ead3332a
Benjamin Vetter <vetter@flakks.com>
2012-11-05 09:07:43 +0100

    Tiny logging fix.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/java -classpath jetty/*:lire/*:bin Similarity 2>&1 | /usr/bin/tee log/similarity.log
+/usr/bin/java -classpath jetty/*:lire/*:bin Similarity 2>&1 | /usr/bin/tee -a log/similarity.log
 
